5cc61d72da44110ebd334b4316e0b4023661cad2
dipak2016 <tops1111@Macs-iMac.local>
2016-06-03 16:27:50 +0530

    this is side bar menu
    side bar menu

---
+++ AboutusViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutusViewController.h
+//  SideBarApp
+//
+//  Created by Mac on 6/3/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SWRevealViewController.h"
+@interface AboutusViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *sidebar;
+
+@end
---
+++ AboutusViewController.m
@@ -0,0 +1,48 @@
+//
+//  AboutusViewController.m
+//  SideBarApp
+//
+//  Created by Mac on 6/3/16.
+//  Copyright © 2016 Mac. All rights reserved.
+//
+
+#import "AboutusViewController.h"
+
+@interface AboutusViewController ()
+
+@end
+
+@implementation AboutusViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.title = @"About Us";
+    
+    // Change button color
+    _sidebar.tintColor = [UIColor colorWithWhite:0.1f alpha:0.9f];
+    
+    // Set the side bar button action. When it's tapped, it'll show up the sidebar.
+    _sidebar.target = self.revealViewController;
+    _sidebar.action = @selector(revealToggle:);
+    
+    // Set the gesture
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
